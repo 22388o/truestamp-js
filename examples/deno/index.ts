e4f493f04ebd637d4c0c14289f75bdb4c79a79b2
Glenn Rempe <glenn@truestamp.com>
2021-09-09 16:11:12 -0400

    Update Deno example to include wrapping all calls in try/catch

--- examples/deno/index.ts
+++ examples/deno/index.ts
@@ -9,7 +9,7 @@
 // Or, load from local lib in development
 import Truestamp from "../../dist/truestamp.module.js"
 
-import { createHash } from "https://deno.land/std@0.103.0/hash/mod.ts"
+import { createHash } from "https://deno.land/std@0.106.0/hash/mod.ts"
 
 // setup Truestamp client using local config file
 const configTxt = await Deno.readTextFile("../config.json")
@@ -17,8 +17,12 @@ const config = JSON.parse(configTxt)
 const t = new Truestamp(config)
 
 // check for health
-const h = await t.getHealth()
-console.log(h)
+try {
+  const h = await t.getHealth()
+  console.log(h)
+} catch (error) {
+  console.error(error)
+}
 
 // submit a new document
 const now = new Date()
@@ -26,19 +30,37 @@ const nowHasher = createHash("sha256")
 nowHasher.update(now.toISOString())
 const nowHashBase64 = nowHasher.toString("base64")
 
-const newDoc = await t.createDocument({
-  hash: nowHashBase64,
-  name: "sha2-256",
-})
-console.log(newDoc)
+let newDoc
+try {
+  newDoc = await t.createDocument({
+    hash: nowHashBase64,
+    name: "sha2-256",
+  })
+  console.log(newDoc)
+} catch (error) {
+  console.error(error)
+}
+
+if (!newDoc) {
+  console.log("No new document created")
+  Deno.exit(1)
+}
 
 // retrieve document
-const newDocRetrieved = await t.getDocument(newDoc.id)
-console.log(newDocRetrieved)
+try {
+  const newDocRetrieved = await t.getDocument(newDoc.id)
+  console.log(newDocRetrieved)
+} catch (error) {
+  console.error(error)
+}
 
 // list all documents owned by this user
-// const allDocs = await t.getAllDocuments({ start: "2021-04-21" })
-// console.log(allDocs)
+// try {
+//   const allDocs = await t.getAllDocuments({ start: "2021-04-21" })
+//   console.log(allDocs)
+// } catch (error) {
+//   console.error(error)
+// }
 
 // update document
 const later = new Date()
@@ -46,16 +68,28 @@ const laterHasher = createHash("sha256")
 laterHasher.update(later.toISOString())
 const laterHashBase64 = laterHasher.toString("base64")
 
-const updatedDoc = await t.updateDocument(newDoc.id, {
-  hash: laterHashBase64,
-  name: "sha2-256",
-})
-console.log(updatedDoc)
+try {
+  const updatedDoc = await t.updateDocument(newDoc.id, {
+    hash: laterHashBase64,
+    name: "sha2-256",
+  })
+  console.log(updatedDoc)
+} catch (error) {
+  console.error(error)
+}
 
 // get all versions for doc
-const docVersions = await t.getDocumentVersions(newDoc.id)
-console.log(docVersions)
+try {
+  const docVersions = await t.getDocumentVersions(newDoc.id)
+  console.log(docVersions)
+} catch (error) {
+  console.error(error)
+}
 
 // delete document
-const deletedDoc = await t.deleteDocument(newDoc.id)
-console.log(deletedDoc) // should be 204 no response empty {}
+try {
+  const deletedDoc = await t.deleteDocument(newDoc.id)
+  console.log(deletedDoc) // should be 204 no response empty {}
+} catch (error) {
+  console.error(error)
+}
